6d7c60b3829972f97b14e3eb5978faaa9d3edd00
Naveen akkineni <naveenakkineni63@gmail.com>
2025-05-10 13:20:33 +0530

    Time: 5 ms (48.29%), Space: 56.6 MB (25.15%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,16 +1,21 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        double sum = 0;
-    for (int i = 0; i < k; ++i)
-      sum += nums[i];
-    double ans = sum;
-
-    for (int i = k; i < nums.length; ++i) {
-      sum += nums[i] - nums[i - k];
-      ans = Math.max(ans, sum);
-    }
-
-    return ans / k;
+        double maxSum=Integer.MIN_VALUE;
+        int i=0;int j=0;
+        int Sum = 0;
+        while(j<nums.length){
+            Sum += nums[j];
+            if(j-i+1<k){
+                j++;
+            }
+            else if(j-i+1==k){
+                maxSum=Math.max(Sum,maxSum);
+                Sum -=nums[i];
+                i++;
+                j++;
+            }
+        }
+        return maxSum/k;
         
         
         
