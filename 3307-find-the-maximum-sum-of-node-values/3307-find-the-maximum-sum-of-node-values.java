6efcbbe706dda0baefd4fa691f52cba83dc5600f
Naveen akkineni <naveenakkineni63@gmail.com>
2025-05-23 09:39:42 +0530

    Time: 36 ms (5.26%), Space: 55.2 MB (35.53%) - LeetHub

--- 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
@@ -1,17 +1,40 @@
-class Solution {
-    public long maximumValueSum(int[] nums, int k, int[][] edges) {
-        long maxSum = 0;
-    int changedCount = 0;
-    int minChangeDiff = Integer.MAX_VALUE;
+class Solution 
+{
+    public long maximumValueSum(int[] nums, int k, int[][] edges) 
+    {
+        // Step 1: Compute the base sum
+        long baseSum = 0;
+        List<Integer> gains = new ArrayList<>();
 
-    for (final int num : nums) {
-      maxSum += Math.max(num, num ^ k);
-      changedCount += ((num ^ k) > num) ? 1 : 0;
-      minChangeDiff = Math.min(minChangeDiff, Math.abs(num - (num ^ k)));
-    }
+        // Step 2: Compute gain for each node if XORed with k
+        for (int num : nums) 
+        {
+            int xorVal = num ^ k;
+            int gain = xorVal - num;
+            baseSum += num;
+            gains.add(gain);
+        }
 
-    if (changedCount % 2 == 0)
-      return maxSum;
-    return maxSum - minChangeDiff;
+        // Step 3: Sort gains in descending order
+        gains.sort(Collections.reverseOrder());
+
+        // Step 4: Try taking top even number of gains to maximize gain
+        long maxGain = 0, currentGain = 0;
+        int count = 0;
+
+        for (int g : gains) 
+        {
+            currentGain += g;
+            count++;
+        
+            // Step 5: Only accept the gain if we have added even number of gains
+            if (count % 2 == 0) 
+            {
+                maxGain = Math.max(maxGain, currentGain);
+            }
+        }
+
+        // Step 6: Return final result
+        return baseSum + maxGain;
     }
 }
